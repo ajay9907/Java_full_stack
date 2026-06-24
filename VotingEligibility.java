226583b21b2683742b691a00634f7c9e7db2c3e9
Ajay Agwan <agwanajay28@gmail.com>
2026-06-24 23:09:27 +0530

    Add files via upload

---
+++ VotingEligibility.java
@@ -0,0 +1,14 @@
+//Write a program to check if a person is eligible for voting (age ≥ 18).
+
+public class VotingEligibility {
+	public static void main(String[] args) {
+		int age = 90;
+
+		if (age >= 18) {
+			System.out.println("Youre eligible for voting ");
+		} else {
+
+			System.out.println("not Eligible for voting");
+		}
+	}
+}
