5be17c04ae5564f4b25af6089ac9277506285b7f
Ajay Agwan <agwanajay28@gmail.com>
2026-07-05 22:01:35 +0530

    Add files via upload

--- PracticeSumofDigit.java
+++ PracticeSumofDigit.java
@@ -1,15 +1,15 @@
-//Find the sum of digits of a number
+//Find the sum of digits of a number-->1345=1+3+4+5=13
 
 public class PracticeSumofDigit {
 	public static void main(String[] args) {
-		int n = 20;
+		int num = 1345;//-->ans 10
 		int sum = 0;
-		while (n > 0) {
-			int digit = n % 10;
-			sum = sum + digit;
-			n = n / 10;
+		while (num> 0) {
+			int d=num%10;//1234
+			sum=sum+d;//0+1234=1234
+			num/=10;//1234/
 
-		}
-		System.out.println(sum);
+		}System.out.println(sum);
+
 	}
 }
