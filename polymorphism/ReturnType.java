308f3da62c5ce30a85534424c804d9ee60968fc3
Ajay Popat  Agwan <agwanajay28@gmail.com>
2026-07-22 17:05:21 +0530

    Add files via upload

---
+++ polymorphism/ReturnType.java
@@ -0,0 +1,17 @@
+package com.polymorphism;
+
+//14. Write a program to show that return type alone cannot overload a method.
+
+public class ReturnType {
+
+	public int display() {
+		return 10;
+	}
+
+	public double display() {
+		return 20.0;
+	}
+
+	
+
+}
